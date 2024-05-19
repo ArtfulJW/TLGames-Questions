313c36dc11af545f09af97a60ed28bc340ee002d
Jay Wang <jwang550@my.bcit.ca>
2024-05-18 22:39:44 -0700

    Finished Question 2 & 3

--- TechnicalQuestions(1-4).lua
+++ TechnicalQuestions(1-4).lua
@@ -48,12 +48,12 @@ function printSmallGuildNames(memberCount)
   local guildName = result.getString("name")
   -- Just to make sure, we're going to iterate through them all manually.
   for k,v in pairs(guildName) do
-    print( v )
+    print(v)
 end
 
 -- ======= Question 3 =======
 -- Assumedly, I can observe that this function tries to remove a specific member from the player's party by Name (membername). I will rename this function appropriately to removePartymemberByName
-function removePartymemberByName(playerId, membername)\
+function removePartymemberByName(playerId, membername)
   
   -- Create local player variable and save a Player with the given playerID.
   local player = Player(playerId)
@@ -62,14 +62,17 @@ function removePartymemberByName(playerId, membername)\
   
   -- For each player in the party, check their names. I'm assuming that getMembers() returns a table of strings.
   for k,v in pairs(party:getMembers()) do
-    -- My assumption is that party:getMembers() returns a table that we iterate through its key-value pairs. 
-    -- The check for the player's name is rather odd, I question the need to create a new Player just to check the name, when we can just simply do a string comparison (assuming that Names are unique).
-    if v == membername then
+    -- The check for the player's name is rather odd, there's no need to create a new Player just to check the name, when we can just simply do a string comparison (assuming that Names are unique).
+    if v:getName() == membername then
       party:removeMember(Player(membername))
+      -- print("Removed: " .. v:getName() .. " from your party")
+      return true
     end
     
   end
-  
+  -- If we can't find and remove the member, we immediately exit
+  -- print("Unable to remove:" .. membername .. "as they are not in your party")
+  return false
 end
 
 -- ======= Question 4 =======
